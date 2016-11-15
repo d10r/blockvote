5ffa918b8350ea797621909ab7e1f04bd8a55602
didi <didi@bikecitizens.net>
2016-11-15 22:29:43 +0100

    added script which pre-funds voters accounts

---
+++ app/server/gas-distributor.js
@@ -0,0 +1,4 @@
+// Ethereum currently doesn't allow contracts to pay for execution.
+// As a workaround clients can call this script with an Ethereum address as parameter in order to get some funding.
+// This workaround should become obsolete with the Ethereum Serenity release, see https://blog.ethereum.org/2016/03/05/serenity-poc2/ (Gas checking)
+
