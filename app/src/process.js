8f3c891ce22b78ef96f8131be72e5adf4db9f3a8
sstarke <sirsupertramp@gmail.com>
2016-11-16 00:26:29 +0100

    Added process scroll area.

--- app/src/process.js
+++ app/src/process.js
@@ -1,6 +1,30 @@
 export class Processing {
 
-    constructor() {
+  constructor() {
+    this.rows = [];
+    this.i = 0;
+    this.fill();
+  }
 
+  fill() {
+    for (var i = 0; i < 30; i++) {
+      this.i += i;
+      this.rows.push({
+        msg: "Stimme für van der Bellen " + this.i
+      });
+      this.rows.push({
+        msg: "Stimme für van der Wuf " + this.i
+      });
+      this.rows.push({
+        msg: "Stimme vergessen " + this.i
+      });
     }
+  }
+
+  refresh() {
+    this.fill()
+    $('.scrollable').stop().animate({
+      scrollTop: $('.scrollable')[0].scrollHeight
+    }, 800);
+  }
 }
